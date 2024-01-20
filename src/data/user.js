e6aeea3fc5fb48e61bcb85194f5f48516f7a987e
RecountsXxx <bogdankapriyanua@gmail.com>
2024-01-20 11:44:49 +0000

    update text

--- src/data/user.js
+++ src/data/user.js
@@ -13,7 +13,7 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Junior Back-end developer",
+		title: "Bogdan Kapriyan | Junior Back-end developer",
 		description:
 			"I am a backend developer with expertise in Laravel. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
